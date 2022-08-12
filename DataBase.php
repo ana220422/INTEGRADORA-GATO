0d110abd9201caa2fa07189a454f0c2a0fa8a1cd
ana220422 <111159287+ana220422@users.noreply.github.com>
2022-08-12 18:55:57 -0500

    Add files via upload
    Nuevos cambios

---
+++ DataBase.php
@@ -0,0 +1,29 @@
+<?php
+class database{
+    private $hostname="localhost";
+    private $database="makeupgold";
+    private $username="root";
+    private $password="root123";
+    private $charset="utf8";
+
+
+    function conectar(){
+        try{
+        $conexion = "mysql:host=" . $this->hostname . "; dbname=" . $this->database . "; charset=" .$this->charset ;
+        $options=[
+            PDO::ATTR_ERRMOT => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_EMULATE_PREPARES => false
+        ];
+
+        $pdo= new PDO($conexion,$this->$username,$this->$password,$options);
+
+        return $pdo;
+    }
+    catch(PDOException $e){
+echo 'Error conexion: ' . $e-> getMessage();  
+exit;
+    }
+}
+}
+    
+?>
